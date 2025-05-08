0df1212f0491a1be06aa7486889400efae302f16
TieByn26 <dxuantienn@gmail.com>
2025-05-08 16:11:00 +0700

    feat: ux ui recruiter #1

---
+++ app/src/main/java/com/example/findjob/ui/components/button/Button.kt
@@ -0,0 +1,4 @@
+package com.example.findjob.ui.components.button
+
+class button {
+}
---
+++ app/src/main/java/com/example/findjob/ui/components/card/RecruiterCard.kt
@@ -0,0 +1,8 @@
+package com.example.findjob.ui.components.card
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Recruiter () {
+
+}
